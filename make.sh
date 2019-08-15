ea78694c48cb1f1cc0079508aad30e08e1ae0be4
Joe Zou <yixian.zou@gmail.com>
2019-08-16 00:31:47 +0800

    fix make.sh

--- make.sh
+++ make.sh
@@ -12,6 +12,7 @@ go get github.com/zouyx/agollo
 
 echo "打包中.."
 bin_name=agollo-agent
+addition_file="app.properties seelog.xml"
 
 echo "making linux_64..."
 GOOS=linux   GOARCH=amd64 go build -o ./dist/${bin_name}_linux_64 main/main.go
@@ -32,7 +33,23 @@ echo "making darwin_64..."
 GOOS=darwin  GOARCH=amd64 go build -o ./dist/${bin_name}_darwin_64 main/main.go
 
 echo "构建运行环境.."
-mkdir build
-cp -rf $projectName build/
-cp -rf seelog.xml build/
-cp -rf app.properties build/
+cp -rf seelog.xml dist/
+cp -rf app.properties dist/
+cd ./dist
+echo "taring linux_64..."
+tar -zcvf ${bin_name}_linux_64.gz ${bin_name}_linux_64 ${addition_file}
+
+echo "taring linux_386..."
+tar -zcvf ${bin_name}_linux_386.gz ${bin_name}_linux_386 ${addition_file}
+
+echo "taring windows_386..."
+tar -zcvf ${bin_name}_windows_386.gz ${bin_name}_windows_386.exe ${addition_file}
+
+echo "taring windows_64..."
+tar -zcvf ${bin_name}_windows_64.gz ${bin_name}_windows_64.exe ${addition_file}
+
+echo "taring darwin_386..."
+tar -zcvf ${bin_name}_darwin_386.gz ${bin_name}_darwin_386 ${addition_file}
+
+echo "taring darwin_64..."
+tar -zcvf ${bin_name}_darwin_64.gz ${bin_name}_darwin_64 ${addition_file}
